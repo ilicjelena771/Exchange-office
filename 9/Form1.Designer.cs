dc3543658e62d36a7b9e7d741cb40ea9511e820e
ilicjelena771 <ilic.jelena771@gmail.com>
2021-10-27 21:24:38 +0200

    Project 2

---
+++ 9/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Konverzija_valuta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.rez = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 407);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(103, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label1.Location = new System.Drawing.Point(32, 441);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Konverzija";
+            
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label2.Location = new System.Drawing.Point(520, 441);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Iznos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label3.Location = new System.Drawing.Point(273, 441);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "U valutu:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label4.Location = new System.Drawing.Point(161, 441);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 18);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Iz valute:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.SeaGreen;
+            this.button1.Location = new System.Drawing.Point(370, 406);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 27);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "izracunaj";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.ForeColor = System.Drawing.Color.SeaGreen;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "RSD",
+            "EUR",
+            "USD",
+            "GBP",
+            "ATS",
+            "DEM",
+            "DKK",
+            "JPY",
+            "AUD",
+            "CAD",
+            "NOK",
+            "RUB",
+            "CHF",
+            "SEK"});
+            this.comboBox1.Location = new System.Drawing.Point(149, 407);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(91, 26);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.ForeColor = System.Drawing.Color.SeaGreen;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "RSD",
+            "EUR",
+            "USD",
+            "GBP",
+            "ATS",
+            "DEM",
+            "DKK",
+            "JPY",
+            "AUD",
+            "CAD",
+            "NOK",
+            "RUB",
+            "CHF",
+            "SEK"});
+            this.comboBox2.Location = new System.Drawing.Point(262, 407);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(91, 26);
+            this.comboBox2.TabIndex = 8;
+            
+            // 
+            // rez
+            // 
+            this.rez.Location = new System.Drawing.Point(492, 406);
+            this.rez.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rez.Name = "rez";
+            this.rez.Size = new System.Drawing.Size(103, 21);
+            this.rez.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(11, 336);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(601, 63);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(620, 481);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.rez);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Konverzije valuta";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox rez;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 9/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//definisemo strukturu 
+struct ValInfo
+{ 
+    public double RSDtoVal;
+    public double ValToRSD;
+    public string ImagePath;
+   
+   
+    private double v;
+    private double v1;
+    private string v2;
+    //konstruktor samo generise
+
+    public ValInfo(double v, double v1, string v2) : this()
+    {
+        this.v = v;
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+};
+
+
+namespace Konverzija_valuta
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //pravimo mapu
+        Dictionary<string, ValInfo> valute = new Dictionary<string, ValInfo>()
+
+            {
+                { "AUD", new ValInfo{ RSDtoVal=0.01132, ValToRSD=88.3394,ImagePath="australia.png" } },
+                { "ATS", new ValInfo{ RSDtoVal=0.1110, ValToRSD=9.0103, ImagePath="austria.png"} },
+                { "BAM", new ValInfo{ RSDtoVal=0.01577, ValToRSD=63.3922, ImagePath="bosna.png"} },
+                { "DKK", new ValInfo{ RSDtoVal=0.06003, ValToRSD=16.6583, ImagePath="danska.png"} },
+                { "EUR", new ValInfo{ RSDtoVal=0.0081, ValToRSD=123.9844, ImagePath="eu.jpg"} },
+                { "JPY", new ValInfo{ RSDtoVal=0.9644, ValToRSD=1.03691, ImagePath="japan.jpg"} },
+                { "CAD", new ValInfo{ RSDtoVal=0.01157, ValToRSD=86.4424,ImagePath= "kanada.png"} },
+                { "CNY", new ValInfo{ RSDtoVal=0.05981, ValToRSD=16.7203, ImagePath="kina.png"} },
+                { "DEM", new ValInfo{ RSDtoVal=0.01577, ValToRSD=63.3922, ImagePath="nemacka.jpg"} },
+                { "NOK", new ValInfo{ RSDtoVal=0.0739, ValToRSD=13.5322, ImagePath="norveska.png"} },
+                { "RUB", new ValInfo{ RSDtoVal=0.49208, ValToRSD=2.0322, ImagePath="rusija.png"} },
+                { "USD", new ValInfo{ RSDtoVal=0.0087, ValToRSD=115.3129,ImagePath= "sad.gif"} },
+                { "CHF", new ValInfo{ RSDtoVal=0.0087, ValToRSD=115.6032, ImagePath="svajcarska.gif"} },
+                { "SEK", new ValInfo{ RSDtoVal=0.07709, ValToRSD=12.9717, ImagePath="svedska.png"}},
+                { "GBP", new ValInfo{ RSDtoVal=0.00697, ValToRSD=143.4175, ImagePath="britanija.jpg"} }
+
+            };
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //inicijalizujemo vrednosti gde ce da nam stoji prvi picturebox i odgovarajuce labele
+            
+            int odLeve = 40, odGore = 40;
+            int vOdLeve = 65,  vOdGore= 95;
+            int iOdLeve = 55, iOdGore = 110;
+
+            //u ovoj petlji za svaku vrednost iz mape pravimo pictureboxove i labele
+            foreach (KeyValuePair<string, ValInfo> item in valute)
+            {
+                PictureBox p1 = new PictureBox();
+                p1.Location = new Point(odLeve, odGore);
+                p1.Size = new Size(80, 50);
+                p1.BackgroundImage = Image.FromFile(item.Value.ImagePath);
+                p1.BackgroundImageLayout = ImageLayout.Stretch;
+                this.Controls.Add(p1);
+
+                Label l1 = new Label();
+                l1.Location = new Point(vOdLeve, vOdGore);
+                l1.Size = new Size(50, 15);
+                l1.Text = item.Key;
+                l1.ForeColor = Color.SeaGreen;
+                this.Controls.Add(l1);
+
+                Label ll1 = new Label();
+                ll1.Location = new Point(iOdLeve, iOdGore);
+                ll1.Size = new Size(50, 15);
+                ll1.Text = item.Value.ValToRSD.ToString();
+                ll1.ForeColor = Color.SeaGreen;
+                this.Controls.Add(ll1);
+                //svaku sledecu vrednost pomeramo u desno, tj. pomeramo od leve ivice za 115
+                odLeve += 115;
+                vOdLeve += 115;
+                iOdLeve += 115;
+                //u jednom redu treba da se nadje 5 valuta,
+                //sto znaci ako nam je vrednost od leve ivice postala veca od 500 prelazimo u novi red
+                if (odLeve > 500)
+                {
+                    odLeve = 40;
+                    vOdLeve = 65;
+                    iOdLeve = 55;
+                    odGore += 100;
+                    vOdGore += 100;
+                    iOdGore += 100;
+                }
+
+           }
+
+       }
+
+        //radimo samo konverziju iz dinara u stranu valutu i obrnuto
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != "RSD")
+            {
+                comboBox2.Text = "RSD";
+                comboBox2.Enabled = false;
+
+            }
+            else
+            {
+                comboBox2.Enabled = true;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double novac;//ono sto konvertujemo
+            double kurs;//koliki je kurs
+            double rezultat;//sta dobijemo pri konverziji
+            if (double.TryParse(textBox1.Text, out novac))
+            {
+                //ako menjamo iz strane valute u dinare
+                if (comboBox1.SelectedItem != "RSD")
+                {
+
+                    kurs = valute[comboBox1.SelectedItem.ToString()].ValToRSD;//iz mape izvucemo kurs
+                    rezultat = novac * kurs;
+                    rez.Text = rezultat.ToString();
+
+                }
+                //ako menjamo novac iz dinara u stranu valutu
+                else if (comboBox1.SelectedItem == "RSD")
+                {
+                    if (comboBox2.SelectedItem != "RSD")
+                    {
+                        kurs = valute[comboBox2.SelectedItem.ToString()].RSDtoVal;//iz mape izvucemo kurs
+                        rezultat = novac * kurs;
+                        rez.Text = rezultat.ToString();
+                    }
+                    else {
+                        rez.Text = novac.ToString();//ako unesemo iz rsd u rsd ostaje ista vrednost
+                    }
+                }                
+            }
+            else
+            {
+                MessageBox.Show("Neispravan unos kolicine novca.","Greska");
+            }
+        }        
+    }
+}
